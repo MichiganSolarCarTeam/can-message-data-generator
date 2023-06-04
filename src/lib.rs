0db530f0e9b230daeb56ec06a702929760d62a08
Shourya Bansal <bansalsh@umich.edu>
2023-06-04 14:45:56 -0400

    Change Project Name

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod signal_type;
 use pyo3::prelude::*;
 
 #[pymodule]
-fn simulated_signal_data(_py: Python, m: &PyModule) -> PyResult<()> {
+fn can_message_data_generator(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<signal_type::SignalType>()?;
     m.add_class::<signal_generator::SignalGenerator>()?;
     m.add_function(wrap_pyfunction!(signal_generator::get_max_limit, m)?)?;
